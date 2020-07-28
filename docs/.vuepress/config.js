95b08b3b9bd0b6547c3727e0dd2dfd3319dc2f9f
bai.zixv <bai.zixv@foxmail.com>
2020-07-28 11:22:58 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     [
       'script',
       {},
-      `<script>
+      `
       var _hmt = _hmt || [];
       (function() {
         var hm = document.createElement("script");
@@ -23,7 +23,6 @@ module.exports = {
         var s = document.getElementsByTagName("script")[0]; 
         s.parentNode.insertBefore(hm, s);
       })();
-      </script>
       `
     ]
   ],
