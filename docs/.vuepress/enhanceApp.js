a6ff84fc8f05fe792c5295cc6a77479a369d7f74
bai.zixv <bai.zixv@foxmail.com>
2020-07-29 17:56:20 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,23 +2,23 @@ export default ({ router }) => {
 	/**
 	 * 路由切换事件处理
 	 */
-	// router.beforeEach((to, from, next) => {
-	// 	console.log('切换路由', to.fullPath, from.fullPath)
+	router.beforeEach((to, from, next) => {
+		// console.log('切换路由', to.fullPath, from.fullPath)
 
-	// 	//触发百度的pv统计
-	// 	if (typeof _hmt != 'undefined') {
-	// 		if (to.path) {
-	// 			_hmt.push(['_trackPageview', to.fullPath])
-	// 			console.log('上报百度统计', to.fullPath)
-	// 		}
-	// 	}
+		//触发百度的pv统计
+		if (typeof _hmt != 'undefined') {
+			if (to.path) {
+				_hmt.push(['_trackPageview', `/bookmarks${to.fullPath}`])
+				// console.log('上报百度统计', to.fullPath)
+			}
+		}
 
-	// 	// continue
-	// 	next()
-	// })
+		// continue
+		next()
+	})
 }
 
-/** 监控
+/** 监控1
  * 首次访问
  * 刷新页面
  * 切换到当前文章的其它章节
